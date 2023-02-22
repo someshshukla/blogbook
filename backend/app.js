75e9c7f65c33af5694237138e9f5025a237c2436
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-23 02:07:44 +0530

    connected to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,21 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 const app = express();
 
-app.use("/", (req, res, next) =>{
-    res.send("aiyo")
-});
 
 
-app.listen(5000);
+mongoose
+    .connect(
+    'mongodb+srv://admin:NuF1e85R3Z5lBT4u@cluster0.ds4eheq.mongodb.net/?retryWrites=true&w=majority'
+)
+.then(() => app.listen(6000))
+.then(() => console.log("connected to DB and listening to localhost 5000")
+)
+.catch((err) => console.log(err));
+
+
+
+
+
+
+//NuF1e85R3Z5lBT4u
