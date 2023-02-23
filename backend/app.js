15e07592cce56f4b800bc0d5ad03434eba0ece69
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-23 20:04:46 +0530

    1

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ mongoose
     .connect(
     'mongodb+srv://admin:NuF1e85R3Z5lBT4u@cluster0.ds4eheq.mongodb.net/?retryWrites=true&w=majority'
 )
-.then(() => app.listen(6000))
+.then(() => app.listen(5000))
 .then(() => console.log("connected to DB and listening to localhost 5000")
 )
 .catch((err) => console.log(err));
