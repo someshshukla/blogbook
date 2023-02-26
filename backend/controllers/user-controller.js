9777a973da3457a8053a87702f7d543cc3c4fbf8
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-26 21:51:51 +0530

    implemented login

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -41,4 +41,27 @@ export const signup = async (req, res, next) => {
         console.log(err);
     }
     return res.status(201).json({user})
+};
+
+export const login = async(req, res, next) => {
+    const {name, email, password} = req.body;
+    let existingUser;
+    try{
+        existingUser = await User.findOne({email})
+    }catch(err){
+       return console.log(err);
+    }
+    if(!existingUser){
+        return res
+        .status(404)
+        .json({message: "No user exists with this email!"});
+
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if(!isPasswordCorrect){
+        return res.status(400).json({message:"Incorrect Password"})
+    }
+    return res.status(200).json({message:"Login Successfull"})
+    
 }
