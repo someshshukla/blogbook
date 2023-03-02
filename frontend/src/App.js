41308793fdf4783f4f0e921e898db292aca4dc9c
Somesh Shukla <somesh13shukla@gmail.com>
2023-03-02 21:40:53 +0530

    built the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
+import Header from "./components/Header";
+
+
 function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+  return <Header/>;
 }
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material';
+
 const Header = () => {
   return (
-    <AppBar>
+    <AppBar sx={{background:'#4267B2'}}>
       <Toolbar>
-        <Typography>blogbook</Typography>
+        <Typography variant ="h4">blogbook</Typography>
+        <Box display = "flex" marginLeft="auto">
+          <Button sx={{ margin:1, borderRadius: 10 }} color="warning">Login</Button>
+          <Button sx={{ margin:1, borderRadius: 10 }} color="warning">Signup</Button>
+        </Box>
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
 export default Header;
