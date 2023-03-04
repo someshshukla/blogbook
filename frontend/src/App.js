bb5974fe7503ac5923e0c1a8c0ee851044860a81
Somesh Shukla <somesh13shukla@gmail.com>
2023-03-04 22:12:28 +0530

    created routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,23 @@
 import Header from "./components/Header";
+import React from "react";
+import { Routes } from "react-router-dom";
 
 
 function App() {
-  return <Header/>;
+  return <React.Fragment>
+    <header>
+      <Header/>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/auth" element={<Auth />}/>
+        <Route path="/blogs" element={<Blogs />}/>
+        <Route path="/myBlogs" element={<UserBlogs />}/>
+        <Route path="/myBlogs/:id" element={<BlogDetail />}/>
+        <Route path="/blogs/add" element={<AddBlog />}/>
+      </Routes>
+    </main>
+  </React.Fragment>;
 }
 
 export default App;
