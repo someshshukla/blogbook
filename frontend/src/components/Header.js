ad8f7b19ef20dad9f3f62df2f21b20cc23676707
Somesh Shukla <somesh13shukla@gmail.com>
2023-03-06 11:54:26 +0530

    built the login page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import { AppBar, Toolbar, Typography, Box, Button, Tabs, Tab } from '@mui/material';
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 
 const Header = () => {
+  const isLoggedIn = useSelector(state=> state.isLoggedIn);
+
   const [value, setvalue] = useState();
   return (
     <AppBar
@@ -11,16 +14,16 @@ const Header = () => {
     sx={{background:'#4267B2'}}>
       <Toolbar>
         <Typography variant ="h4">blogbook</Typography>
-        <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
+        { isLoggedIn && <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
           <Tabs textColor='inherit' value={value} onChange={(e, val)=>setvalue(val)}>
             <Tab LinkComponent={Link} to="/blogs" label="All Blogs"/>
             <Tab LinkComponent={Link} to="/myblogs" label="My Blogs"/>
           </Tabs>
-        </Box>
+        </Box>}
         <Box display = "flex" marginLeft="auto">
-          <Button LinkComponent={Link} to="/auth" sx={{ margin:1, borderRadius: 10 }} color="warning">Login</Button>
-          <Button LinkComponent={Link} to="/auth" sx={{ margin:1, borderRadius: 10 }} color="warning">Signup</Button>
-          <Button LinkComponent={Link} to="/auth" sx={{ margin:1, borderRadius: 10 }} color="warning">Logout</Button>
+          { !isLoggedIn &&<Button LinkComponent={Link} to="/auth" sx={{ margin:1, borderRadius: 10 }} color="warning">Login</Button>}
+          { !isLoggedIn &&<Button LinkComponent={Link} to="/auth" sx={{ margin:1, borderRadius: 10 }} color="warning">Signup</Button>}
+          { isLoggedIn && <Button LinkComponent={Link} to="/auth" sx={{ margin:1, borderRadius: 10 }} color="warning">Logout</Button>}
         </Box>
       </Toolbar>
     </AppBar>
