470ca7b08bbbc1ea828560f088e846fa0975b6e5
Somesh Shukla <somesh13shukla@gmail.com>
2023-03-06 11:54:36 +0530

    built the auth page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,14 @@ import AddBlog from "./components/AddBlog";
 
 
 import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth";
+import { useSelector } from "react-redux";
 
 
 function App() {
+  const isLoggedIn = useSelector(state=> state.isLoggedIn);
+  console.log(isLoggedIn);
   return <React.Fragment>
     <header>
       <Header/>
--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,8 +1,30 @@
+import { TextField, Box, Button, Typography } from '@mui/material';
 import React from 'react'
 
 const Auth = () => {
   return (
-    <div>Auth</div>
+    <div>
+      <form>
+        <Box display="flex"
+        maxWidth={400}
+         flexDirection={'column'}
+         alignItems={"center"}
+         justifyContent={'center'}
+         boxShadow="10px 10px 20px #ccc"
+         padding={3}
+         margin='auto'
+         marginTop={5}
+         borderRadius={5}
+        >
+          <Typography padding={3} textAlign='center'>Login</Typography>
+          <TextField margin='normal'/>
+          <TextField margin='normal'/>
+          <TextField margin='normal'/>
+          <Button>Submit</Button>
+          <Button>Signup</Button>
+        </Box>
+      </form>
+    </div>
   )
 }
 
