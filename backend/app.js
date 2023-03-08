55a36899b1637aeb9fcc3a8736b83b36dc6bd142
Somesh Shukla <somesh13shukla@gmail.com>
2023-03-08 19:30:16 +0530

    a lot of stuff

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import blogRouter from "./routes/user-routes";
 import router from "./routes/user-routes";
-
+import cors from 'cors';
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/user', router);
 app.use("/api/user", blogRouter);
--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,14 +1,48 @@
 import { TextField, Box, Button, Typography } from '@mui/material';
-import React, { useState } from 'react'
-
+import React, { useState } from 'react';
+import axios from "axios";
 const Auth = () => {
-  const [state, setstate] = useState({
-    name:"",email:"",password:""
+  const [inputs, setInputs] = useState({
+    name:"",
+    email:"",
+    password:""
   });
   const [isSignup, setIsSignUp] = useState(false);
+  const handleChange = (e) => {
+    setInputs((prevState)=>({
+      ...prevState,
+      [e.target.name] : e.target.value
+    }))
+  };
+
+  const sendRequest = async (type="login") => {
+    const res = await axios.post(`http://localhost:5000/api/user/${type}`, {
+      name: inputs.name,
+      email: inputs.email,
+      password: inputs.password
+    }).catch(err=>console.log(err));
+
+    const data = await res.data;
+    return data;
+
+    
+  }
+
+  
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    console.log(inputs);
+
+    if(isSignup){
+      sendRequest("signup").then(data=>console.log(data))
+    } else {
+      sendRequest().then(data=>console.log(data))
+    }
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Box display="flex"
         maxWidth={400}
          flexDirection={'column'}
@@ -21,10 +55,31 @@ const Auth = () => {
          borderRadius={5}
         >
  <Typography variant='h4' padding={3} textAlign='center'>Login</Typography>
-{ isSignup &&<TextField placeholder='Name' margin='normal'/>
-}          <TextField type={"email"} placeholder='Email' margin='normal'/>
-          <TextField type={"password"} placeholder='password' margin='normal'/>
-          <Button variant='contained' sx={{marginTop: 3}}>Submit</Button>
+{ isSignup && 
+          <TextField 
+          name='name' 
+          onChange={handleChange} 
+          value={inputs.name} 
+          placeholder='Name' 
+          margin='normal'/>
+}         <TextField 
+          name='email' 
+          onChange={handleChange} 
+          value={inputs.email} 
+          type={"email"} 
+          placeholder='Email' 
+          margin='normal'/>
+          <TextField name='password' 
+          onChange={handleChange} 
+          value={inputs.password} 
+          type={"password"} 
+          placeholder='password' 
+          margin='normal'/>
+          <Button type='submit'
+          variant='contained' 
+          sx={{marginTop: 3}}>
+            Submit
+          </Button>
           <Button sx={{marginTop: 3}} onClick={()=>setIsSignUp(!isSignup)}>{isSignup ? "Login" : "Signup"}</Button>
         </Box>
       </form>
