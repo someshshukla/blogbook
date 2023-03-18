7ec6a5f4aedf626040c30581ba92523264faffa1
Somesh Shukla <somesh13shukla@gmail.com>
2023-03-18 22:18:02 +0530

    data

--- frontend/src/components/Blogs.js
+++ frontend/src/components/Blogs.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect} from 'react'
+import axios from "axios";
+
 
 const Blogs = () => {
+  const [blogs, setblogs] = useState();
+  const sendRequest  = async() => {
+    const res = await axios.get("http://localhost:5000/api/blog").catch(err => console.log(err));
+    const data = await res.data;
+    return data;
+  }
+  useEffect(() => {
+    sendRequest().then(data=>console.log(data));
+
+  }, [])
+  
   return (
     <div>Blogs</div>
   )
